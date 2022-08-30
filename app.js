e2f5244176f3f3780290f77f44e13d7a99c8c99a
01ccccccc <cindy92376@gmail.com>
2022-08-30 17:08:13 +0800

    [Feature] Add error handler

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT;
 
 app.set('view engine', 'pug');
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+// Handle 404
+app.use(function (req, res, next) {
+  // console.log('req.query: ', req.query);
+  console.log('404', req.url);
+  return res.status(404).json({ error: 'error: 404' });
+});
+
+//Handle 500
+app.use(function (err, req, res, next) {
+  console.log('error handler: ', err);
+  return res.status(500).render('error', { msg: 'error: 500' });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}!`);
 });
