1472249d9b2ff21b0c940f10f2ae95e02c49af1e
01ccccccc <cindy92376@gmail.com>
2022-08-30 16:56:14 +0800

    [Feature] Build index page

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
 });
