58ee41cf1a087c8b615824e56426f79057f4f7e0
01ccccccc <cindy92376@gmail.com>
2022-09-04 16:09:39 +0800

    [Feature] API test can iterate througuout the whole test case table

--- controllers/testcase_controller.js
+++ controllers/testcase_controller.js
@@ -6,134 +6,216 @@ const saveCase = async (req, res) => {
   let result = testExample(req.body.featureCode);
   // console.log('result.testTableBody: ', result.testTableBody);
 
-  // TODO: for loop to iterate all test data in table
-  let data = JSON.stringify(result.testTableBody[0]);
-  //   console.log('result.testTableBody[0]: ', result.testTableBody[0]);
+  let actualResponseArray = [];
 
-  let config = {
-    method: req.body.httpMethod,
-    url: `${req.body.domainName}${req.body.apiEndpoint}`,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: data,
-    timeout: 1000,
-  };
+  for (let i = 0; i < result.testTableBody.length; i++) {
+    let testData = JSON.stringify(result.testTableBody[i]);
+    // console.log('testData: ', testData);
 
-  axios(config)
-    .then(function (response) {
-      // TODO: verify whether the test passed or not
-      let actualResult = response.data;
-      let testTime = new Date().toUTCString();
+    let config = {
+      method: req.body.httpMethod,
+      url: `${req.body.domainName}${req.body.apiEndpoint}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: testData,
+      timeout: 1000,
+    };
 
-      const awesome_instance = new testCaseModel({
-        // TODO: to automatically generate all the id?
-        // TODO: to input all the data from real test case
-        user_id: 1,
-        test_id: 1,
-        project_id: 1,
-        collection_id: 1,
-        api_id: 1,
-        domain_name: req.body.domainName,
-        http_method: req.body.httpMethod,
-        api_endpoint: req.body.apiEndpoint,
-        test_record: {
-          request: {
-            title: 'User',
-            description: 'user system to make user sign up and sign in',
-            tags: [],
-            scenario: result.testStep,
-            test_cases: [
-              {
-                case_id: 1,
-                test_case: result.testTableBody[0],
-                expected_result: {
-                  response_body: {},
-                  status_code: result.testTableBody[0].status,
+    await axios(config)
+      .then(function (response) {
+        // TODO: verify whether the test passed or not
+        let actualResult = response.data;
+        let testTime = new Date().toUTCString();
+
+        actualResponseArray.push({
+          case_id: 1,
+          response_data: actualResult,
+          response_status: response.status,
+          pass: response.status === Number(result.testTableBody[i].status),
+          request_time: testTime,
+          request_time_length: 1000,
+        });
+        // console.log('actualResponseArray in axios then: ', actualResponseArray);
+      })
+      .catch(function (error) {
+        let actualResult = error.response?.data;
+        let testTime = new Date().toUTCString();
+
+        // console.log('in testcase controller catch error: ', error);
+        actualResponseArray.push({
+          case_id: 1,
+          response_data: actualResult,
+          response_status: error.response?.status,
+          pass:
+            error.response?.status === Number(result.testTableBody[i].status),
+          request_time: testTime,
+          request_time_length: 1000,
+        });
+        // console.log(
+        //   'actualResponseArray in axios catch: ',
+        //   actualResponseArray
+        // );
+      })
+      .finally(function () {
+        const awesome_instance = new testCaseModel({
+          // TODO: to automatically generate all the id?
+          // TODO: to input all the data from real test case
+          user_id: 1,
+          test_id: 1,
+          project_id: 1,
+          collection_id: 1,
+          api_id: 1,
+          domain_name: req.body.domainName,
+          http_method: req.body.httpMethod,
+          api_endpoint: req.body.apiEndpoint,
+          test_record: {
+            request: {
+              title: 'User',
+              description: 'user system to make user sign up and sign in',
+              tags: [],
+              scenario: result.testStep,
+              test_cases: [
+                {
+                  case_id: 1,
+                  test_case: result.testTableBody[0],
+                  expected_result: {
+                    response_body: {},
+                    status_code: result.testTableBody[0].status,
+                  },
                 },
-              },
-            ],
-            severity: 5,
-          },
-          response: [
-            {
-              case_id: 1,
-              response_data: actualResult,
-              response_status: response.status,
-              pass: response.status === Number(result.testTableBody[0].status),
-              request_time: testTime,
-              request_time_length: 1000,
+              ],
+              severity: 5,
             },
-          ],
-        },
-      });
-
-      awesome_instance.save(function (error) {
-        if (error) console.log('instance error', error);
-        else console.log('inserted');
-      });
-      console.log('post call passed');
-      return res.status(200).json({ message: 'inserted' });
-    })
-    .catch(function (error) {
-      console.log(error.response?.data);
-      let actualResult = error.response?.data;
-      let testTime = new Date().toUTCString();
-
-      const awesome_instance = new testCaseModel({
-        // TODO: to automatically generate all the id?
-        user_id: 1,
-        test_id: 1,
-        project_id: 1,
-        collection_id: 1,
-        api_id: 1,
-        domain_name: req.body.domainName,
-        http_method: req.body.httpMethod,
-        api_endpoint: req.body.apiEndpoint,
-        test_record: {
-          request: {
-            title: 'User',
-            description: 'user system to make user sign up and sign in',
-            tags: [],
-            scenario: result.testStep,
-            test_cases: [
-              {
-                case_id: 1,
-                test_case: result.testTableBody[0],
-                expected_result: {
-                  response_body: {},
-                  status_code: result.testTableBody[0].status,
-                },
-              },
-            ],
-            severity: 5,
+            response: actualResponseArray,
           },
-          response: [
-            {
-              case_id: 1,
-              response_data: actualResult,
-              response_status: error.response.status,
-              pass:
-                error.response.status ===
-                Number(result.testTableBody[0].status),
-              request_time: testTime,
-              request_time_length: 1000,
-            },
-          ],
-        },
-      });
+        });
+        // console.log('actualResponseArray in finally: ', actualResponseArray);
 
-      awesome_instance.save(function (error) {
-        if (error) console.log('instance error', error);
-        else console.log('inserted');
+        awesome_instance.save(function (error) {
+          if (error) console.log('instance error', error);
+          else console.log('inserted');
+        });
+        console.log('post call passed');
       });
-      console.log('post call failed');
-      //   console.log('error.response: ', error.response);
-      return res.render('error', {
-        message: error.response.data.status,
-        time: testTime,
-      });
-    });
+    // console.log('actualResponseArray after finally: ', actualResponseArray);
+  }
+  return res.status(200).json({ message: 'inserted' });
+
+  //   axios(config)
+  //     .then(function (response) {
+  //       // TODO: verify whether the test passed or not
+  //       let actualResult = response.data;
+  //       let testTime = new Date().toUTCString();
+
+  //       const awesome_instance = new testCaseModel({
+  //         // TODO: to automatically generate all the id?
+  //         // TODO: to input all the data from real test case
+  //         user_id: 1,
+  //         test_id: 1,
+  //         project_id: 1,
+  //         collection_id: 1,
+  //         api_id: 1,
+  //         domain_name: req.body.domainName,
+  //         http_method: req.body.httpMethod,
+  //         api_endpoint: req.body.apiEndpoint,
+  //         test_record: {
+  //           request: {
+  //             title: 'User',
+  //             description: 'user system to make user sign up and sign in',
+  //             tags: [],
+  //             scenario: result.testStep,
+  //             test_cases: [
+  //               {
+  //                 case_id: 1,
+  //                 test_case: result.testTableBody[0],
+  //                 expected_result: {
+  //                   response_body: {},
+  //                   status_code: result.testTableBody[0].status,
+  //                 },
+  //               },
+  //             ],
+  //             severity: 5,
+  //           },
+  //           response: [
+  //             {
+  //               case_id: 1,
+  //               response_data: actualResult,
+  //               response_status: response.status,
+  //               pass: response.status === Number(result.testTableBody[0].status),
+  //               request_time: testTime,
+  //               request_time_length: 1000,
+  //             },
+  //           ],
+  //         },
+  //       });
+
+  //       awesome_instance.save(function (error) {
+  //         if (error) console.log('instance error', error);
+  //         else console.log('inserted');
+  //       });
+  //       console.log('post call passed');
+  //       return res.status(200).json({ message: 'inserted' });
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error.response?.data);
+  //       let actualResult = error.response?.data;
+  //       let testTime = new Date().toUTCString();
+
+  //       const awesome_instance = new testCaseModel({
+  //         // TODO: to automatically generate all the id?
+  //         user_id: 1,
+  //         test_id: 1,
+  //         project_id: 1,
+  //         collection_id: 1,
+  //         api_id: 1,
+  //         domain_name: req.body.domainName,
+  //         http_method: req.body.httpMethod,
+  //         api_endpoint: req.body.apiEndpoint,
+  //         test_record: {
+  //           request: {
+  //             title: 'User',
+  //             description: 'user system to make user sign up and sign in',
+  //             tags: [],
+  //             scenario: result.testStep,
+  //             test_cases: [
+  //               {
+  //                 case_id: 1,
+  //                 test_case: result.testTableBody[0],
+  //                 expected_result: {
+  //                   response_body: {},
+  //                   status_code: result.testTableBody[0].status,
+  //                 },
+  //               },
+  //             ],
+  //             severity: 5,
+  //           },
+  //           response: [
+  //             {
+  //               case_id: 1,
+  //               response_data: actualResult,
+  //               response_status: error.response.status,
+  //               pass:
+  //                 error.response.status ===
+  //                 Number(result.testTableBody[0].status),
+  //               request_time: testTime,
+  //               request_time_length: 1000,
+  //             },
+  //           ],
+  //         },
+  //       });
+
+  //       awesome_instance.save(function (error) {
+  //         if (error) console.log('instance error', error);
+  //         else console.log('inserted');
+  //       });
+  //       console.log('post call failed');
+  //       //   console.log('error.response: ', error.response);
+  //       return res.render('error', {
+  //         message: error.response.data.status,
+  //         time: testTime,
+  //       });
+  //     });
 };
 
 module.exports = { saveCase };
