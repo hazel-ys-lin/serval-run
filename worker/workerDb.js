2799e5d5479da5b8733235bd18a24d67584e6eed
01ccccccc <cindy92376@gmail.com>
2022-09-29 14:27:38 +0800

    [Fix] Database connection error handling

--- worker/workerDb.js
+++ worker/workerDb.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const uri = process.env.MONGODB_URI;
-const mongodb = mongoose.connect(uri, {
-  useNewUrlParser: true,
-  serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-});
+const mongodb = mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  })
+  .then(() => {
+    console.log('[Worker] Connected to mongoDB!');
+  })
+  .catch((error) => {
+    console.log('[Worker] Connect to mongoDB failed...');
+  });
 // .catch((error) => console.log('mongoDB connection error: ', error.reason));
 
 // mongoose.Promise = global.Promise;
