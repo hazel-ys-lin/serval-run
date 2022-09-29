23e154621532000172c577d132c97969f77cf1f3
01ccccccc <cindy92376@gmail.com>
2022-09-29 14:50:38 +0800

    [Fix] Worker database connection fail bug

--- worker/insertResult.js
+++ worker/insertResult.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
+const { mongodb } = require('./workerDb');
 const momentTimezone = require('moment-timezone');
+const uri = process.env.MONGODB_URI;
+const options = {
+  useNewUrlParser: true,
+  serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+};
 
 const responseSchema = new mongoose.Schema({
   api_id: {
@@ -73,6 +80,13 @@ const responseModel = mongoose.model('response', responseSchema);
 const reportModel = mongoose.model('report', reportSchema);
 
 const exampleResponseInsertModel = async function (responseArray) {
+  try {
+    await mongoose.connect(uri, options);
+    console.log('[Worker] Connected to mongoDB!');
+  } catch (error) {
+    console.log('[Worker] Connect to mongoDB failed...', error);
+    handleError(error);
+  }
   // console.log('responseArray: ', responseArray);
   try {
     for (let i = 0; i < responseArray.length; i++) {
@@ -91,6 +105,7 @@ const exampleResponseInsertModel = async function (responseArray) {
 
     // change the report status to finishied
     // FIXME: what if have two loops (example: collection test)
+    console.log('responseArray: ', responseArray);
     let reportStatusUpdate = await reportModel.findOneAndUpdate(
       { _id: responseArray[0].report_id },
       {
