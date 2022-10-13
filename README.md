e9af663c2aa7b4a87dee9d416c1a1d728414e672
01ccccccc <cindy92376@gmail.com>
2022-10-13 14:57:34 +0800

    [Feature] Add testing account auto completely

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# serval-run
+# ServelRun
 
-- meaningful
+An automated integration testing and document tool for developers and non-developers.
+
+
+## Table of Contents
+- [Features]()
+- [Architecture]()
+- [Usage/Examples]()
+
+## Features
+
+- Running integration testing by 3 levels:
+    - APIs' Colleciton
+    - Single API
+    - Single scenario under an API
+- Compiling [Gherkin](https://cucumber.io/docs/gherkin/) to testable documents
+- Viewing real-time charts of testing results 
+
+
+## Architecture
+
+
+## Usage/Examples
--- test/set_up.js
+++ test/set_up.js
@@ -1,23 +1,23 @@
-const app = require('../socket');
-const chai = require('chai');
-const deepEqualInAnyOrder = require('deep-equal-in-any-order');
-const chaiHttp = require('chai-http');
-const { truncateFakeData, createFakeData } = require('./fake_data_generator');
+// const app = require('../socket');
+// const chai = require('chai');
+// const deepEqualInAnyOrder = require('deep-equal-in-any-order');
+// const chaiHttp = require('chai-http');
+// const { truncateFakeData, createFakeData } = require('./fake_data_generator');
 
-chai.use(chaiHttp);
-chai.use(deepEqualInAnyOrder);
+// chai.use(chaiHttp);
+// chai.use(deepEqualInAnyOrder);
 
-const assert = chai.assert;
-const expect = chai.expect;
-const requester = chai.request(app).keepOpen();
+// const assert = chai.assert;
+// const expect = chai.expect;
+// const requester = chai.request(app).keepOpen();
 
-before(async () => {
-  await truncateFakeData();
-  await createFakeData();
-});
+// before(async () => {
+//   await truncateFakeData();
+//   await createFakeData();
+// });
 
-module.exports = {
-  expect,
-  assert,
-  requester,
-};
+// module.exports = {
+//   expect,
+//   assert,
+//   requester,
+// };
